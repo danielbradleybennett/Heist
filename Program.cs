b580fef04afbdcbf0a54165e940bf1f18f120f3e
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-03-02 13:49:58 -0600

    phase4

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace Heist {
 
             Console.WriteLine ("Plan a Heist");
             List<Dictionary<string, string>> teamList = new List<Dictionary<string, string>> ();
+            int bankDifficulty = 100;
+            int allSkillLevel = 0;
 
             while (true) {
+
                 Dictionary<string, string> teamMember = new Dictionary<string, string> ();
                 Console.WriteLine ("Enter team member's name");
                 string memberName = Console.ReadLine ();
+
                 if (memberName == "") {
                     break;
 
@@ -27,16 +31,34 @@ namespace Heist {
                     string memberCourage = Console.ReadLine ();
                     teamMember.Add ("level", memberCourage);
 
-                    foreach (KeyValuePair<string, string> member in teamMember) {
-                        Console.WriteLine ($"{member.Key}: {member.Value}");
-
-                    }
-                    teamList.Add (teamMember);
+                    // foreach (KeyValuePair<string, string> member in teamMember) {
+                    //     Console.WriteLine ($"{member.Key}: {member.Value}");
 
                 }
+                teamList.Add (teamMember);
 
             }
+
+            foreach (Dictionary<string, string> member in teamList) {
+                foreach (KeyValuePair<string, string> pair in member) {
+                    if (pair.Key == "skill") {
+
+                        int skillLevel = int.Parse (pair.Value);
+                        allSkillLevel += skillLevel;
+
+                    }
+                }
+            }
+
             Console.WriteLine (teamList.Count);
+            Console.WriteLine (allSkillLevel);
+
+            if (bankDifficulty > allSkillLevel) {
+                Console.WriteLine ("You are all dead");
+            } else {
+                Console.WriteLine ("You may be able to pull this off");
+            }
+
         }
     }
 }
